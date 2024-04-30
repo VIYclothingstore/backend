be4bd6042cc05bdfe006f7f5f99aa12d5c034c1f
vitayushchyk <vitay2715@gmail.com>
2024-04-30 17:29:34 +0300

    endpoint ping is implemented

--- src/users/views.py
+++ src/users/views.py
@@ -1,9 +1,14 @@
+from django.http import JsonResponse
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from users.models import Role, User
 from users.serializers import UserCreateSerializer, UserRetrieveUpdateDestroySerializer
+
+
+def ping(request):
+    return JsonResponse({'message': 'Hello from Django!'})
 
 
 class UserCreateAPIView(CreateAPIView):
@@ -21,4 +26,3 @@ class UserRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
         if request.user.role != Role.ADMIN:
             raise PermissionDenied("Arrrrr, you shall not pass!!!! ")
         return super().delete(request, *args, **kwargs)
-
