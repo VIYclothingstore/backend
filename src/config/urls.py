2d4b005d7acbb8d6c3d034387a3df45ba6389841
Mykola <kolia.chaun@icloud.com>
2024-05-01 15:51:50 +0300

    swagger added in project

--- src/config/urls.py
+++ src/config/urls.py
@@ -4,8 +4,12 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 from users import views
 from users.views import UserCreateAPIView, UserRetrieveUpdateDestroyAPIView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    # API
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 
     path("ping/", views.ping, name="Hello world"),
     # ADMIN
