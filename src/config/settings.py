070bbceafc14358c64c84d43562e8ca94c2cb856
vitayushchyk <vitay2715@gmail.com>
2024-05-01 19:43:58 +0300

    fix token

--- src/config/settings.py
+++ src/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "users",
     "corsheaders",
     "drf_spectacular",
+    "products",
 ]
 
 THIRD_PARTY_APPS = [
@@ -150,13 +151,24 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
+
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "UPDATE_LAST_LOGIN": True,
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=100),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=40),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": False,
+
     "ALGORITHM": "HS256",
-    "SIGNING_KEY": SECRET_KEY,
+
     "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+    "TOKEN_OBTAIN_SERIALIZER": "user.serializers.CustomTokenObtainPairSerializer"
 }
+
 
 APPEND_SLASH = False
--- src/config/urls.py
+++ src/config/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from users import views
-from users.views import UserCreateAPIView, UserRetrieveUpdateDestroyAPIView
+from users.views import UserCreateAPIView, UserRetrieveUpdateDestroyAPIView, CustomTokenObtainPairView, \
+    CustomTokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -15,7 +16,8 @@ urlpatterns = [
     # ADMIN
     path("admin/", admin.site.urls),
     # TOKEN
-    path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("token/refresh/", CustomTokenRefreshView.as_view(), name="token-refresh"),
     # USER
     path("user/", UserCreateAPIView.as_view(), name="user-create"),
     path(
