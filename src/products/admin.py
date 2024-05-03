85b5296e7c06a31472d3fd6729a7b385c48343df
vitayushchyk <vitay2715@gmail.com>
2024-05-03 19:12:03 +0300

    added product model

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, ProductCategory
+
+admin.site.register(Product)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(ProductCategory)
+
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
--- src/products/models.py
+++ src/products/models.py
@@ -1,23 +1,29 @@
-# from django.db import models
-# from storages.backends.gcloud import GoogleCloudStorage
-#
-#
-# class ProductPhotoStorage(GoogleCloudStorage):
-#     bucket_name =
-#
-# class ProductCategory(models.Model):
-#     name = models.CharField(max_length=100)
-#     description = models.TextField()
-#
-# class Product(models.Model):
-#     name = models.CharField(max_length=100)
-#     description = models.TextField()
-#     price = models.DecimalField(max_digits=5, decimal_places=2)
-#     image = models.ImageField(upload_to='products_images/')
-#
-#     category = models.ForeignKey(
-#         ProductCategory,
-#         on_delete=models.CASCADE,
-#         related_name='products',
-#     )
-#
+from django.db import models
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=100)
+    value = models.IntegerField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+    available_color = models.ManyToManyField(Color, blank=True)
+    category = models.ManyToManyField(ProductCategory, related_name='products', ),
+    is_for_women = models.BooleanField(default=False)
+    is_for_men = models.BooleanField(default=False)
+
+
+class WarehouseItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    size = models.IntegerField()
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    is_available = models.BooleanField(default=False)
