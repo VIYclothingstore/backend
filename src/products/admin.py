47297abc236e89c467fe7fd8850a6b0b739a6344
vitayushchyk <vitay2715@gmail.com>
2024-06-16 19:42:15 +0300

    storage of photos in the Google bucket is implemented

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Category, Product
 
 
 @admin.register(Product)
 class IssueAdmin(admin.ModelAdmin):
-
     list_display = ["id", "name", "price"]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    search_fields = ["name"]
+    list_filter = ["name"]
--- src/products/migrations/0004_rename_colors_product_color.py
+++ src/products/migrations/0004_rename_colors_product_color.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("products", "0003_remove_category_slug_alter_product_sizes"),
     ]
