928b38d97502539be67e472eb0096477b198ba56
vitayushchyk <vitay2715@gmail.com>
2024-07-30 08:55:52 +0300

    - added page pagination

--- src/products/views.py
+++ src/products/views.py
@@ -2,6 +2,7 @@ import random
 
 from django.db.models import Q
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
@@ -19,6 +20,8 @@ class ProductRetrieveAPIView(generics.RetrieveAPIView):
 class ProductListAPIView(generics.ListAPIView):
     queryset = ProductItem.objects.filter()
     serializer_class = ProductSerializer
+    pagination_class = PageNumberPagination
+    pagination_class.page_size = 8
 
 
 class AvailableProductStockAPIView(APIView):
