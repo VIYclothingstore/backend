d23b29446f258aa43b11455b9d1a21badba37c04
vitayushchyk <vitay2715@gmail.com>
2024-07-30 09:59:36 +0300

    - added default pagination in settings.py

--- src/products/views.py
+++ src/products/views.py
@@ -2,7 +2,6 @@ import random
 
 from django.db.models import Q
 from rest_framework import generics
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
@@ -20,14 +19,6 @@ class ProductRetrieveAPIView(generics.RetrieveAPIView):
 class ProductListAPIView(generics.ListAPIView):
     queryset = ProductItem.objects.filter()
     serializer_class = ProductSerializer
-    pagination_class = LimitOffsetPagination
-    default_limit = 8
-
-    def paginate_queryset(self, queryset):
-        self.limit = self.request.query_params.get("limit", self.default_limit)
-        if self.limit is None:
-            return None
-        return super().paginate_queryset(queryset)
 
 
 class AvailableProductStockAPIView(APIView):
