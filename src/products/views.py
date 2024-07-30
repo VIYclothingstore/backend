e434afca2b44a721c11b0c9943718eb912876d35
vitayushchyk <vitay2715@gmail.com>
2024-07-30 10:50:52 +0300

    - added pagination to search product

--- src/products/views.py
+++ src/products/views.py
@@ -2,6 +2,7 @@ import random
 
 from django.db.models import Q
 from rest_framework import generics
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
@@ -34,6 +35,8 @@ class AvailableProductStockAPIView(APIView):
 
 
 class ProductSearchView(APIView):
+    pagination_class = LimitOffsetPagination
+
     def get(self, request):
 
         gender = request.GET.get("gender")
