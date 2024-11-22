5053d6277be90d655c1d319350c98bacd5fd65df
Mykola <kolia.chaun@icloud.com>
2024-11-22 12:18:22 +0200

    Added execution of unit tests in GitHub Actions #27

--- src/config/settings_test.py
+++ src/config/settings_test.py
@@ -1,12 +1,19 @@
-from pathlib import Path
+# src/config/settings_test.py
 
-from .settings import *
+from .settings import *  # Импортируем все настройки из основного файла
+import os
+from google.oauth2 import service_account
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    '/Users/jabko/Desktop/projects/backend/gcloud-secret.json'
+    
+)
+# Настройки базы данных для тестов
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # Используем SQLite для тестов
     }
 }
+
+# Если настройки для базы данных уже есть в settings.py, то их можно не переписывать. Они будут переопределены
